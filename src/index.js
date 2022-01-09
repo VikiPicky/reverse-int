cecdc7030d6a60273c5f204a6f23e4aefbd36888
VikiPicky <v.mitiakova@gmail.com>
2022-01-09 18:08:42 -0500

    init: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    return reverseString(n.toString().replace("-", ""));
+}
+
+function reverseString(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
 }
